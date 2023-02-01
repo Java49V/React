cc6c8a52944b3fa0f8e3a5c33ce538f41e356e28
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-02-01 22:43:09 +0300

    CW #38 React-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Life } from './components/Life';
-import lifeConfig from './config/life-game.json';
-import './App.css';
 
+import './App.css'
 function App() {
-  return (
-    <div style={{ display: 'flex' }}>
-      <Life
-        dim={lifeConfig.dimentions}
-        ticInterval={lifeConfig.ticInterval}
-      ></Life>
-    </div>
-  );
+  
+
+  return <div></div>
+
 }
 
 export default App;
