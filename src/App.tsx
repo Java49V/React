d839099a25ff922bff5e1b78f168b3fa56eb43db
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-02-12 16:31:41 +0300

    CW #40 HW #40 Reusable Navigator solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { Navigator } from './components/navigators/Navigator';
 import './App.css'
-import { Layout } from './components/navigators/Layout';
 import { Products } from './components/navigators/Products';
 import { BreadProducts } from './components/pages/BreadProducts';
 import { Customers } from './components/pages/Customers';
 import { DairyProducts } from './components/pages/DairyProducts';
 import { Home } from './components/pages/Home';
 import { Orders } from './components/pages/Orders';
+import { layoutConfig } from './config/layout-config';
+import { productsConfig } from './config/products-config';
 
 function App() {
   return <BrowserRouter>
       <Routes>
-          <Route path='/' element={<Layout/>}>
+          <Route path='/' element={<Navigator className={layoutConfig.className}
+           routes={layoutConfig.routes}  />}>
               <Route index element={<Home/>}></Route>
               <Route path='customers' element={<Customers/>}/>
               <Route path='orders' element={<Orders/>}></Route>
-              <Route path='products' element={<Products/>}>
+              <Route path='products' element={<Navigator
+               className={productsConfig.className} routes={productsConfig.routes}/>}>
                     <Route path='dairy' element={<DairyProducts/>}/>
                     <Route path='bread' element={<BreadProducts/>}/>
               </Route>
