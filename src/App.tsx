9d25bb190099e9e109ce33ea2287110637e40bef
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-02-15 16:57:37 +0300

    HW #41 Initial code of Employees application

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navigator } from './components/navigators/Navigator';
 import './App.css'
-import { Products } from './components/navigators/Products';
-import { BreadProducts } from './components/pages/BreadProducts';
-import { Customers } from './components/pages/Customers';
-import { DairyProducts } from './components/pages/DairyProducts';
-import { Home } from './components/pages/Home';
-import { Orders } from './components/pages/Orders';
+
 import { layoutConfig } from './config/layout-config';
-import { productsConfig } from './config/products-config';
+import { Employees } from './components/pages/Employees';
+import { AddEmployee } from './components/pages/AddEmployee';
+import { AgeStatistics } from './components/pages/AgeStatistics';
+import { SalaryStatistics } from './components/pages/SalaryStatistics';
+
 
 function App() {
   return <BrowserRouter>
       <Routes>
           <Route path='/' element={<Navigator className={layoutConfig.className}
            routes={layoutConfig.routes}  />}>
-              <Route index element={<Home/>}></Route>
-              <Route path='customers' element={<Customers/>}/>
-              <Route path='orders' element={<Orders/>}></Route>
+              <Route index element={<Employees/>}/>
+              <Route path='add' element={<AddEmployee/>}/>
+              <Route path='statistics/age' element={<AgeStatistics/>}/>
+              <Route path='statistics/salary' element={<SalaryStatistics/>}/>
               
           </Route>
               
--- src/components/navigators/Navigator.tsx
+++ src/components/navigators/Navigator.tsx
@@ -1,15 +1,15 @@
 import { Link, Outlet } from "react-router-dom";
 import { NavigatorProps } from "../../model/NavigatorProps"
 import '../navigators/navigators.css'
-import {Box, AppBar, Tabs, Tab} from "@mui/material"
+import { Box, AppBar, Tabs, Tab } from "@mui/material"
 import React from "react";
 export const Navigator: React.FC<NavigatorProps> = ({ className, routes }) => {
     const [tabNumber, setTabNumber] = React.useState(0);
     function changeTabNumber(event: any, newNumber: number) {
         setTabNumber(newNumber);
     }
-    return <Box sx={{marginTop: "15vh"}}>
-        <AppBar sx={{backgroundColor: "lightgray"}}>
+    return <Box sx={{ marginTop: "15vh" }}>
+        <AppBar sx={{ backgroundColor: "lightgray" }}>
             <Tabs value={tabNumber} onChange={changeTabNumber} >
                 {getNavItems(routes)}
             </Tabs>
@@ -19,12 +19,5 @@ export const Navigator: React.FC<NavigatorProps> = ({ className, routes }) => {
 }
 function getNavItems(routes: { path: string; label: string }[]): React.ReactNode {
     return routes.map((r, index) => <Tab component={Link} to={r.path}
-     label={r.label} key={index}/>)
-}
-function getActiveProps(isActive: boolean) : React.CSSProperties {
-    let res: React.CSSProperties = {};
-    if (isActive) {
-        res = {backgroundColor: "blue", color: "white", fontSize: "1.2em"}
-    }
-    return res;
+        label={r.label} key={index} />)
 }
